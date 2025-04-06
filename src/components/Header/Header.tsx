5520fe69ad93d41662b50f98baaf11f2507fc41f
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-04-06 19:29:53 +0200

    🎨 add breakpoints to header and detailsproject component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,12 +12,12 @@ function Header() {
   const pathname = usePathname()
 
   return (
-    <header className="flex flex-row justify-around items-center h-[3.5rem] mb-4 overflow-hidden border-b border-transparent shadow-[var(--shadow-custom)]">
+    <header className="flex flex-row md:justify-around sm:justify-between items-center h-[3.5rem] mb-4 overflow-hidden border-b border-transparent shadow-[var(--shadow-custom)]">
       <Link href="/">
-      <Image src={logo} alt="image logo" width={80} height={80}/>
+      <Image src={logo} alt="image logo" width={70} height={70} className='sm:w-[70px] sm:h-[28px] md:w-[70px] md:h-[70px]'/>
       </Link>
       {/* <span className="flex justify-center items-center">XeniaDev </span> */}
-      <nav className="flex flex-row justify-end items-center gap-4">
+      <nav className="flex flex-row justify-end items-center gap-4 sm:hidden md:flex">
         {menuItems.map(item => (
           <MenuLinks
             key={item.name}
--- src/components/Projects/DetailsProject/DetailsProject.tsx
+++ src/components/Projects/DetailsProject/DetailsProject.tsx
@@ -33,18 +33,18 @@ function DetailsProject() {
     selectedProject?.demo && window.open(selectedProject.demo, '_blank')
 
   return (
-    <section className="flex flex-col mx-auto xl:w-[75rem] sm:w-[25rem] sm:mx-auto">
+    <section className="flex flex-col mx-auto xl:w-[75rem] md:w-[35rem] lg:w-[50rem] sm:w-[25rem] sm:mx-auto">
       <CustomButton
         type="button"
         text="Back"
         icon={<FaArrowLeft className="ml-2 w-5" />}
         color="violet"
         onClick={handelBackClick}
-        customCss="!w-[6rem] !text-sm !font-normal mt-4 sm:w-[5rem] sm:text-xs sm:font-light"
+        customCss="md:!w-[6rem] !text-sm !font-normal mt-4 sm:w-[5rem] sm:text-xs sm:font-light"
       />
 
       <div key={selectedProject.id} className="mt-[6rem]">
-        <div className="xl:max-w-[40%] sm:w-[95%] h-auto overflow-hidden rounded-lg shadow-lg box-border mb-6">
+        <div className="md:max-w-[80%] sm:w-[95%] h-auto overflow-hidden rounded-lg shadow-lg box-border mb-6">
           <Image
             src={selectedProject.image}
             alt={selectedProject.title}
@@ -59,20 +59,20 @@ function DetailsProject() {
         <h3 className="text-lg font-semibold text-[var(--text-primary)] mb-4">
           Description :
         </h3>
-        <p className="text-base font-medium text-[var(--text-secondary)] w-[60%] sm:w-[90%] mb-6">
+        <p className="text-base font-medium text-[var(--text-secondary)] xl:w-[60%] lg:w-[60%] md:w-[70%] sm:w-[90%] mb-6">
           {selectedProject.description}
         </p>
 
-        <h3 className="text-lg font-semibold text-[var(--text-primary)] mb-2">
+        <h3 className="text-lg font-semibold text-[var(--text-primary)] mb-2 sm:mb-4">
           Technologies used :
         </h3>
-        <div className="grid grid-cols-4 gap-4 sm:grid-cols-2 sm:mx-auto">
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 md:gap-4 sm:grid-cols-2 sm:place-items-center sm:gap-4">
           {selectedProject.technos.map(tech => (
             <MagicButton key={tech} title={tech} />
           ))}
         </div>
          <hr className="my-10 border-t border-[var(--text-secondary)]"/>
-        <div className="flex md:flex-row gap-4 mt-8 mb-8 sm:flex-row sm:items-center">
+        <div className="flex md:flex-row md:justify-start gap-4 mt-8 mb-8 sm:flex sm:flex-row sm:justify-center sm:items-center">
           <CustomButton
             text="Github"
             color="violet"

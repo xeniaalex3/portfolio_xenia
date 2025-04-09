965390971eb0b029836184ce1353155100018b36
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-04-09 16:58:46 +0200

    ⚡️format code

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,6 @@
-import { Suspense } from 'react';
+import { Suspense } from 'react'
 import DetailsProject from '@/components/Projects/DetailsProject/DetailsProject'
-import { ProjectDetailsData } from '@/data/projects_data';
+import { ProjectDetailsData } from '@/data/projects_data'
 
 interface DetailPageProps {
   params: Promise<{
@@ -12,16 +12,16 @@ export default async function ProjectDetails(props: DetailPageProps) {
   const { projectId } = await props.params
 
   const project = ProjectDetailsData.find(
-    (project) => project.id === Number(projectId)
-  );
+    project => project.id === Number(projectId)
+  )
 
   if (!project) {
-    return <div>Project not found.</div>;
+    return <div>Project not found.</div>
   }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <DetailsProject project={project}/>
-      </Suspense>
-  ) 
+      <DetailsProject project={project} />
+    </Suspense>
+  )
 }

2c52bc4547e906ae06194e4063de8a14de53f8f6
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-12-15 18:39:40 +0100

    ✨ add new project images and update project details in ProjectsData

--- src/data/projects_data.ts
+++ src/data/projects_data.ts
@@ -1,18 +1,36 @@
-import image1 from '@/assets/image/planntrip3.png'
-import image2 from '@/assets/image/feedback1.png'
-import image3 from '@/assets/image/esports.png'
-
+import image2 from '@/assets/image/plannTrip.png'
+import image3 from '@/assets/image/feedback1.png'
+import image4 from '@/assets/image/esports.png'
+import image1 from '@/assets/image/téléchargement.svg'
 
 export const ProjectsData = [
   {
     id: 1,
-    title: 'PlannTrip',
+    title: 'Taskly',
     image: image1,
+    technos: [
+      'Next.js',
+      'TypeScript',
+      'TanStack',
+      'Authentication',
+      'Prisma',
+      'PostgreSQL',
+      'Tailwind CSS',
+      'CI/CD',
+      'Github Actions'
+    ]
+  },
+  {
+    id: 2,
+    title: 'PlannTrip',
+    image: image2,
     github_link: 'https://github.com/xeniaalex3/PlannTrip',
     demo: 'https://planntrip.netlify.app/',
     technos: [
       'React.js',
       'TypeScript',
+      'TanStack',
+      'Authentication',
       'Nest.js',
       'Prisma',
       'PostgreSQL',
@@ -24,9 +42,9 @@ export const ProjectsData = [
     ]
   },
   {
-    id: 2,
+    id: 3,
     title: 'Feedback Widget',
-    image: image2,
+    image: image3,
     github_link: 'https://github.com/xeniaalex3/Feedback-Widget',
     demo: 'https://feedback-widget-xi-snowy.vercel.app/',
     className: true,
@@ -42,9 +60,9 @@ export const ProjectsData = [
     ]
   },
   {
-    id: 3,
+    id: 4,
     title: 'eSports',
-    image: image3,
+    image: image4,
     github_link: 'https://github.com/xeniaalex3/eSports',
     demo: 'https://e-sports-phi.vercel.app/',
     technos: [
@@ -59,42 +77,61 @@ export const ProjectsData = [
 ]
 
 export const ProjectDetailsData = [
-  {
+   {
     id: 1,
+    title: 'Taskly',
+    image: image1,
+  },
+  {
+    id: 2,
     title: 'PlannTrip',
     description:
-      "Le PlannTrip est une application web destiné à faciliter l'organisation de voyages en groupe. Dotée d'une interface intuitive et moderne, elle permet aux utilisateurs de créer des plans de voyage personnalisés, de choisir des destinations, de fixer des dates et d'inviter des amis par e-mail à collaborer à l'organisation.",
-    image: image1,
+      'PlannTrip est une application web moderne dédiée à la planification de voyages, permettant aux utilisateurs d’organiser des séjours, gérer les participants, planifier des activités et partager des liens utiles. L’application intègre une authentification complète, des routes protégées et une interface responsive, offrant une expérience fluide, performante et orientée collaboration.',
+    image: image2,
     features: [
-      'Création de voyages avec titre, dates et destinations',
-      'Invitation de participants par e-mail',
-      "Ajout d'activités avec date et heure",
-      'Création de liens utiles (par exemple, hébergement, transport, événements)',
-      'Confirmation de la participation des invités',
-      'Synchronisation et collaboration en temps réel'
+      'Création et gestion de voyages (destination, dates)',
+      'Ajout et gestion des participants',
+      'Planification des activités avec date et heure',
+      'Création, modification et suppression des activités',
+      'Partage de liens utiles liés au voyage',
+      'Authentification des utilisateurs (inscription et connexion)',
+      'Gestion du compte utilisateur (profil, mise à jour, suppression)',
+      'Routes protégées pour les utilisateurs authentifiés',
+      'Interface responsive (mobile et desktop)'
     ],
     technos: {
       frontend: [
-        'React.js',
+        'React 19',
         'TypeScript',
+        'Vite',
         'Tailwind CSS',
-        'React-Toastify',
-        'React-Router',
+        'TanStack React Router',
+        'TanStack React Query',
+        'Zod',
+        'React-hook-form',
+        'Axios',
         'CI/CD',
         'Github Actions'
       ],
-      backend: ['Nest.js', 'TypeScript', 'Prisma', 'PostgreSQL', 'Docker', 'Docker compose']
+      backend: [
+        'Nest.js',
+        'TypeScript',
+        'Prisma',
+        'PostgreSQL',
+        'Docker',
+        'Docker compose'
+      ]
     },
     github_link: 'https://github.com/xeniaalex3/PlannTrip',
     backend_repo: 'https://github.com/xeniaalex3/PlannTrip_backend',
     demo: 'https://planntrip.netlify.app/'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Feedback Widget',
     description:
       'The project is called Feedback Widget. It is a tool where the user can leave feedback in case of a problem, an idea or other, this tool allows the user to describe his problem/idea/other along with the screenshot functionality.',
-    image: image2,
+    image: image3,
     features: [
       'Accessible and styled forms',
       ' Screenshot capture functionality',
@@ -117,11 +154,11 @@ export const ProjectDetailsData = [
     demo: 'https://feedback-widget-xi-snowy.vercel.app/'
   },
   {
-    id: 3,
+    id: 4,
     title: 'eSports',
     description:
       'The project is called Find your Duo. It is a platform to find the perfect partner to play your favorite game together.',
-    image: image3,
+    image: image4,
     features: [
       'Modern, component-based UI architecture',
       'Fully responsive and utility-first styling system',
